cb7fc7157f4542818130c774a8f1771267c32b79
Kahlawy <Kahlawy@gmail.com>
2023-03-26 10:27:02 +0300

    Add project files.

---
+++ Cascading Dropdown List ASP .NET Core 7 with SQL Server Database/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Cascading_Dropdown_List_ASP_.NET_Core_7_with_SQL_Server_Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cascading_Dropdown_List_ASP_.NET_Core_7_with_SQL_Server_Database.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cascading Dropdown List ASP .NET Core 7 with SQL Server Database/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cascading_Dropdown_List_ASP_.NET_Core_7_with_SQL_Server_Database.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
