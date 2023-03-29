cb327f5aa61f805b1ab6fa438098defe997a4222
Kahlawy <Kahlawy@gmail.com>
2023-03-29 15:37:41 +0300

    update #5

--- Cascading Dropdown List ASP .NET Core 7 with SQL Server Database/wwwroot/js/site.js
+++ Cascading Dropdown List ASP .NET Core 7 with SQL Server Database/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 //
-// Write your JavaScript code.YouTube Tutrail  https://www.youtube.com/watch?v=vvJhflLovEs
+// Write your JavaScript code.
 //  onchange="Fillcities(this, 'lsCity');"
 
 function Fillcities(lstCountryCtrl, lstCityId) {
